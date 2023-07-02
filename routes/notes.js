15562e36de5b7a7fce27fda108a359e26851eb46
Robyn Smith <robynsmith21@hotmail.com>
2023-07-03 00:52:04 +0100

    finishing get request

--- routes/notes.js
+++ routes/notes.js
@@ -12,3 +12,17 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+// GET Route for a specific tip
+notes.get('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        const  result = json.filter((note) => note.tip_id === tipID);
+        return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID');
+    });
+});
+
+
