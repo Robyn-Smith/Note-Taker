943a01de6b6e832fd5b008d99e2f3f731902c4c9
Robyn Smith <robynsmith21@hotmail.com>
2023-07-03 01:12:16 +0100

    server.js remove uneeded code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
+// const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Import custom middleware, "cLog"
-app.use(clog);
+// app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
@@ -25,14 +25,14 @@ app.get('/', (req, res) =>
 );
 
 // GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // Wildcard route to direct users to a 404 page
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
-);
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+// );
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
